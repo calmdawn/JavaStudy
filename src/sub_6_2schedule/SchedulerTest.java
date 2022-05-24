81c4d6122eaaa1ec126bda436ac1e5a23f1f52bd
calmdawn <qnfqus@gmail.com>
2022-05-25 00:30:19 +0900

    상담원(스케줄러) 인터페이스 완료

--- src/sub_6_2schedule/SchedulerTest.java
+++ src/sub_6_2schedule/SchedulerTest.java
@@ -1,5 +1,6 @@
 package sub_6_2schedule;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class SchedulerTest {
@@ -11,9 +12,22 @@ public class SchedulerTest {
         System.out.println("L : 쉬고 있거나 대기가 가장 적은 상담원에게 할당");
         System.out.println("P : 우선순위가 높은 고객 먼저 할당");
 
+        Scheduler scheduler;
         String command = scan.nextLine();
 
+        if(command.equals("R") || command.equals("r")){
+            scheduler = new RoundRobin();
+        }else if(command.equals("L") || command.equals("l")){
+            scheduler = new LeastJob();
+        }else if(command.equals("P") || command.equals("p")){
+            scheduler = new PriorityAllocation();
+        }else
+            scheduler = null;
 
+        if(scheduler != null){
+            scheduler.getNextCall();
+            scheduler.sendCallToAgent();
+        }
 
     }
 }
